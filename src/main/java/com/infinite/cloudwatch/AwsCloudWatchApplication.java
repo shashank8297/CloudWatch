1a6e94b4614f520485b86c5ba6c42ed28643086f
shashank <shashankc@infinite.com>
2024-04-02 12:58:59 +0530

    Cloud Watch

---
+++ src/main/java/com/infinite/cloudwatch/AwsCloudWatchApplication.java
@@ -0,0 +1,13 @@
+package com.infinite.cloudwatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCloudWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCloudWatchApplication.class, args);
+	}
+
+}
